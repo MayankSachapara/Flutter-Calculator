04c43db2138b04e4b12d924e6cc6c6b1b8d912c2
MayankSachapara <137792267+MayankSachapara@users.noreply.github.com>
2023-10-25 22:20:04 +0530

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # flutter_calculator
 
-A new Flutter project.
+ Flutter Calculator.
 
 ## Getting Started
 
--- lib/model_class.dart
+++ lib/model_class.dart
@@ -2,7 +2,7 @@ class Btn {
   static const String del = "D";
   static const String clr = "C";
   static const String per = "%";
-  static const String multiply = "×";
+  static const String multiply = "*";
   static const String divide = "÷";
   static const String add = "+";
   static const String subtract = "-";
